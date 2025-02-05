6afdd029fed50cd9f0df5456831d6a9aec4cdf5a
Quang LN <quangln2810@gmail.com>
2025-02-05 14:55:10 +0700

    Add new exports for multiCrossplot and multiHistogram; update package version to 0.0.7

--- src/esm.js
+++ src/esm.js
@@ -1,2 +1,4 @@
 export { name as multiWellCrossplot } from './multi-well-crossplot';
+export { name as multiCrossplot } from './multi-crossplot';
 export { name as multiWellHistogram } from './multi-well-histogram';
+export { name as multiHistogram } from './multi-histogram';
